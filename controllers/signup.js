a0e29eecb8529aeed2a19012bf05cb1caeb9cdae
chrisbodhi <cboette@gmail.com>
2014-02-16 16:06:11 -0600

    Attempt to enable user sign up has stalled. Move to using OAuth for sign up.

--- controllers/signup.js
+++ controllers/signup.js
@@ -4,13 +4,16 @@ var SignupModel = require('../models/signup'),
   passport = require('passport'),
   User = require('../models/user'),
   flash = require('connect-flash'),
+  bcrypt = require('bcrypt'),
+  auth = require('../lib/auth'),
   db = require('../lib/database');
 
 // Pulling values from the Signup form
-var name = $('#username').val();
-var email = $('#email').val();
-var password = $('#password').val();
-var confirm = $('#confirmPassword').val();
+// ...this is actually accomplished in auth.js in the signup module. Nice try, past self.
+// var name = $('#username').val();
+// var email = $('#email').val();
+// var password = $('#password').val();
+// var confirm = $('#confirmPassword').val();
 
 module.exports = function (app) {
 
@@ -20,35 +23,39 @@ module.exports = function (app) {
         res.render('signup', model);
     });
 
-    app.post('/signup', function (req, res) {
-      if ( password === confirmPassword ) {
-        var user = new User({
-          name: name,
-          email: email,
-          password: password,
-        });
-        console.log(req);
-        user.save();
-        res.redirect('/todo');
-      } else {
-        console.log(req);
-        model.messages = req.flash('wrong pw');
-        res.render('signup', model);
-      }
-    })
+    // via scotch.io tutorial
 
-    // app.get('/logout', function (req, res) {
-    //   req.logout();
-    //   res.redirect('/');
-    // });
+    // app.post('/signup', passport.authenticate('local-signup', {
+    //   success.redirect('/profile'),
+    //   failure.redirect('/signup'),
+    //   failureFlash: true
+    // }));
+
+    app.post('/signup', auth.signup(), function (req, res) {
+      passport.authenticate('local-signup', {
+        successRedirect: req.session.goingTo || '/profile',
+        failureRedirect: '/signup',
+        failureFlash: true
+      })(req, res);
+    });
+
+    // First attempt - logic needs to go elsewhere
+    // app.post('/signup', function (req, res) {
+    //   if ( password === confirmPassword ) {
+    //     var user = new User({
+    //       name: name,
+    //       email: email,
+    //       password: password,
+    //     });
+    //     console.log(req);
+    //     user.save();
+    //     res.redirect('/todo');
+    //   } else {
+    //     console.log(req);
+    //     model.messages = req.flash('wrong pw');
+    //     res.render('signup', model);
+    //   }
+    // })
 };
 
 
-// var u1 = new User({
-//         name: "Kraken McSquid",
-//         login: "kraken",
-//         password: "password",
-//         role: "admin"
-//     });
-
-// u1.save();
